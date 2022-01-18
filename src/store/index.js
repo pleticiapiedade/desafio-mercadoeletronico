0139d5d0f4b4d05308f089dee193b0ab90bc2599
Leticia <pleticiapiedade@gmail.com>
2022-01-18 12:54:31 -0300

    :card_file_box: API request done on Vuex Store

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,27 @@ import {
 } from 'vuex'
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    card: [],
+  },
+  mutations: {
+    setCard(state, card) {
+      state.card = card;
+    },
+  },
+  actions: {
+    fetchCardData(context) {
+      return fetch("https://me-frontend-challenge-api.herokuapp.com/orders/1")
+        .then((response) => response.json())
+        .then((data) => {
+          context.commit("setCard", data);
+        })
+        .catch((err) => console.error(err));
+    },
+  },
+  getters: {
+    card(state) {
+      return state.countries;
+    },
+  },
 })
